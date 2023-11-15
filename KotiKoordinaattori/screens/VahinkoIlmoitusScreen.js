28bf6dd0a1e6fa0414eebffe881e890f71d6f778
Sampo Ikonen <sampo.ikonen1@gmail.com>
2023-11-15 09:59:01 +0200

    Vahinkoilmoitus versio 1 valmis

--- KotiKoordinaattori/screens/VahinkoIlmoitusScreen.js
+++ KotiKoordinaattori/screens/VahinkoIlmoitusScreen.js
@@ -1,52 +1,142 @@
-import React from 'react';
-import {Alert,Button, SafeAreaView, TextInput,View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import {Alert,TouchableOpacity, SafeAreaView, TextInput,View, Text, StyleSheet, Keyboard, ScrollView } from 'react-native';
+import emailjs from '@emailjs/browser';
+
 
 export default function VahinkoIlmoitusScreen() {
 
-  const [text, onChangeText] = React.useState('');
+  const [user_name, setUserName] = useState('');
+  const [user_email, setUserEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  //Alert the user that the mail is sent
   const sendAlert = () => {
     Alert.alert('Vahinkoilmoitus', 'Lähetetty');
   };
 
+  //Function to check that email is correct
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  //Function to send an email from the contact form
+  const sendEmail = () => {
+
+    //Check if all boxes are filled
+    if (user_name.trim() === '' || user_email.trim() === '' || message.trim() === '') {
+      Alert.alert('Error', 'Please fill in all the required fields.');
+      return;
+    }
+
+    //Check that email is in correct form
+    if (!validateEmail(user_email)) {
+      Alert.alert('Error', 'Please enter a valid email address.');
+      return;
+    }
+
+    //Parameters send to email
+    const templateParams = {
+      user_name,
+      user_email,
+      message,
+    };
+
+    //Emailjs for sending an email to kotikoordinattoritest@gmail.com
+    emailjs.send('service_w5y6a0b', 'template_er8elil', templateParams, '9BTlcOOlVqj2oUvST')
+      .then((result) => {
+        console.log(result);
+        sendAlert();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
+    //Scrollview when you press around the keyboard it hides
+    <ScrollView contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled">
       <Text style={styles.title}>Tee vahinkoilmoitus</Text>
-      <SafeAreaView>
-        < TextInput
-          style={styles.input}
+      <SafeAreaView style={styles.inputContainer}>
+        <View style={styles.inputRow}>
+          {/* Input fields for name and email*/}
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => setUserName(text)}
+            placeholder='Name*'
+          />
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(text) => setUserEmail(text)}
+            placeholder='Email*'
+          />
+        </View>
+          {/* Input Field for message */}
+        <TextInput
+          style={ styles.messageInput}
+          onChangeText={(text) => setMessage(text)}
+          placeholder='Message*'
           multiline={true}
-          numberOfLines={20}
-          onChangeText={onChangeText}
-          value={text}
+          numberOfLines={6}
         />
+        {/* Send button */}
+        <TouchableOpacity style={styles.button} onPress={sendEmail}>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
       </SafeAreaView>
-      <Button title={'Lähetä'} onPress={sendAlert} />
-    </View>
+    </ScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgray', 
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  content: {
-    alignItems: 'center',
+  inputContainer: {
+    width: '80%',
+    alignSelf: 'center',
   },
-  input: {
-    height: 500,
-    width: 400,
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  inputField: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  messageInput: {
+    textAlignVertical: 'top',
+    padding: 10,
+    paddingTop: 10,
+    alignSelf: 'center',
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: '50%',
+    width: '100%',
     margin: 12,
     borderWidth: 1,
+  },
+  button: {
+    backgroundColor: 'blue',
     padding: 10,
-    backgroundColor: 'white',
-    textAlignVertical: 'top'
-  }
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
